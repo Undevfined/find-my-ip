539a3c4bcad48adf4454e1cc616cec8a95a082ef
diegoundevfined <diego.undevfined@gmail.com>
2024-02-02 00:08:25 +0100

    Handle requests coming through cloudflare

--- handlers/ipHandler.go
+++ handlers/ipHandler.go
@@ -7,14 +7,24 @@ import (
 )
 
 func IPHandler(w http.ResponseWriter, r *http.Request) {
-    ip := getIP(r)
-    fmt.Fprintf(w, "%s", ip)
+	ip := getIP(r)
+	fmt.Fprintf(w, "%s", ip)
 }
 
 func getIP(r *http.Request) string {
+    // Check for Cloudflare's CF-Connecting-IP header first
+    cfIP := r.Header.Get("CF-Connecting-IP")
+    if cfIP != "" {
+        return cfIP
+    }
+
+    // Fallback to X-Forwarded-For, which might be set by Cloudflare or other proxies
     forwarded := r.Header.Get("X-FORWARDED-FOR")
     if forwarded != "" {
-        return forwarded
+        ipList := strings.Split(forwarded, ",")
+        return strings.TrimSpace(ipList[0]) 
     }
+
+    // As a last resort, use the IP from the remote connection
     return strings.Split(r.RemoteAddr, ":")[0]
 }
